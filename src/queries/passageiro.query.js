9e7a0c885da5aa18f8fe5df4dcf29ab2cce063fe
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2022-06-05 20:18:20 -0300

    Cleaning up code

--- src/queries/passageiro.query.js
+++ src/queries/passageiro.query.js
@@ -1,6 +1,8 @@
-const INSERIR_PASSSAGEIRO_QUERY = `
+const INSERIR_PASSAGEIRO_QUERY = `
             INSERT INTO passageiro (cpf, nome, sexo, rg, data_nascimento)
             VALUES ($1, $2, $3, $4, $5) RETURNING *
         `;
 
-module.exports = { INSERIR_PASSSAGEIRO_QUERY };
+const LISTAR_PASSAGEIROS_QUERY = "SELECT * FROM passageiro";
+
+module.exports = { INSERIR_PASSAGEIRO_QUERY, LISTAR_PASSAGEIROS_QUERY };
