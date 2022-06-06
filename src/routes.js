6c96e7c4134f4f5c9f32264b93d46d6cb28ad019
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2022-06-05 21:32:13 -0300

    Add routes

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,25 @@
-const { Router } = require('express');
-const PassageiroController = require('./controllers/Passageiro.controller');
-const PassagemController = require('./controllers/Passagem.controller');
-const VoosController = require('./controllers/Voo.controller');
+const { Router } = require("express");
+const PassageiroController = require("./controllers/Passageiro.controller");
+const PassagemController = require("./controllers/Passagem.controller");
+const VoosController = require("./controllers/Voo.controller");
+const PagamentoController = require("./controllers/Pagamento.controller");
+const PassagemController = require("./controllers/Passagem.controller");
 
 const routes = Router();
 
-routes.get('/passageiros', PassageiroController.buscarTodosPassageiros)
-routes.post('/buscar-passageiro', PassageiroController.buscarPassageiro)
-routes.post('/passagens', PassagemController.buscarTodasPassagens)
-routes.post('/buscar-passagem', PassagemController.buscarPassagem)
-routes.post('/voos', VoosController.buscarTodosVoos)
-routes.post('/buscar-voo', VoosController.buscarVoo)
-
-
-
-routes.post('/passageiro',  PassageiroController.store)
-
-
+//endpoints de passageiros
+routes.get("/passageiros", PassageiroController.index);
+routes.post("/passageiro", PassageiroController.store);
+routes.post("/buscar-passageiro", PassageiroController.buscarPassageiro);
+//endpoints de pagamentos
+routes.get("/pagamentos", PagamentoController.index);
+routes.post("/pagamento", PagamentoController.store);
+//endpoints de passagens
+routes.get("/passagens", PassagemController.index);
+routes.post("/passagem", PassagemController.store);
+routes.post("/buscar-passagem", PassagemController.buscarPassagem);
+//endpoints de voos
+routes.post("/voos", VoosController.buscarTodosVoos);
+routes.post("/buscar-voo", VoosController.buscarVoo);
 
 module.exports = routes;
