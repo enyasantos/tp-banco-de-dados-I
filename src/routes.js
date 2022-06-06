a95d9c5cd7597248a460558eb29a4d2a6c390960
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2022-06-05 21:18:22 -0300

    Add routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
-const { Router } = require('express');
-const PassageiroController = require('./controllers/Passageiro.controller');
+const { Router } = require("express");
+const PassageiroController = require("./controllers/Passageiro.controller");
+const PagamentoController = require("./controllers/Pagamento.controller");
 
 const routes = Router();
 
-routes.get('/passageiros', PassageiroController.index)
-routes.post('/passageiro',  PassageiroController.store)
+routes.get("/passageiros", PassageiroController.index);
+routes.post("/passageiro", PassageiroController.store);
+routes.get("/pagamentos", PagamentoController.index);
+routes.post("/pagamento", PagamentoController.store);
 
 module.exports = routes;
