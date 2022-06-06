c51a6a3d50b8cf4df6f3437083252bb0df8b8dbe
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2022-06-05 22:23:20 -0300

    Add routes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ const { Router } = require("express");
 const PassageiroController = require("./controllers/Passageiro.controller");
 const PassagemController = require("./controllers/Passagem.controller");
 const PagamentoController = require("./controllers/Pagamento.controller");
+const BagagemController = require("./controllers/Bagagem.controller");
 const VoosController = require("./controllers/Voo.controller");
+
 const routes = Router();
 
 //endpoints de passageiros
@@ -17,7 +19,10 @@ routes.get("/passagens", PassagemController.index);
 routes.post("/passagem", PassagemController.store);
 routes.get("/buscar-passagem", PassagemController.buscarPassagem);
 //endpoints de voos
-routes.get('/voos', VoosController.buscarTodosVoos)
-routes.get('/buscar-voo', VoosController.buscarVoo)
+routes.get("/voos", VoosController.buscarTodosVoos);
+routes.get("/buscar-voo", VoosController.buscarVoo);
+//endpoints de bagagem
+routes.get("/bagagens", BagagemController.index);
+routes.post("/bagagem", BagagemController.store);
 
 module.exports = routes;
