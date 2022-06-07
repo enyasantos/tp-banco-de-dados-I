87bae1c1c03105e3cc104407a0c22d7eab7df7f6
joaovitorvaz <70914862+joaovitorvaz@users.noreply.github.com>
2022-06-06 23:45:56 -0300

    Update Voo.controller.js

--- src/controllers/Voo.controller.js
+++ src/controllers/Voo.controller.js
@@ -11,20 +11,16 @@ class VoosController {
   }
 
   async buscarVoo(request, response) {
-    const {data} = request.body;
+    const {codigo, data} = request.body;
     connection.query(`
-    SELECT * FROM voo, itinerario, piloto, aviao, comissariobordo, trabalhaem
-      WHERE (
-          voo.data = $1
-          AND (
-              itinerario.codigo = voo.codigo_itinerario
-              AND piloto.cpf = voo.cpf_piloto
-              AND aviao.registro = voo.registro
-              AND trabalhaem.cpf_comissario = comissariobordo.cpf
-              AND voo.codigo = trabalhaem.codigo_voo
-          )
-      )
-    `, [data], 
+      SELECT * FROM voo, itinerario, piloto, aviao, comissariobordo, trabalhaem
+      WHERE (voo.data = $2  OR voo.codigo = $1) 
+      AND itinerario.codigo = voo.codigo_itinerario
+      AND piloto.cpf = voo.cpf_piloto
+      AND aviao.registro = voo.registro
+      AND trabalhaem.cpf_comissario = comissariobordo.cpf
+      AND voo.codigo = trabalhaem.codigo_voo
+    `, [codigo, data], 
     (error, results) => {
       if (error) {
         throw error
